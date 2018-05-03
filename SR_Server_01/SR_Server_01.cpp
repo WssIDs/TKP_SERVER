619196177046f38eb37c62d375d50147744106e0
WssIDs <wssids49@gmail.com>
2018-05-04 02:34:23 +0300

    DllRegister DllUnRegister

--- SR_Server_01/SR_Server_01.cpp
+++ SR_Server_01/SR_Server_01.cpp
@@ -6,9 +6,30 @@
 
 #include <initguid.h>
 #include "CFN_01.h"
+#include "Registry.h"
 
 long    g_lObjs = 0;
 long    g_lLocks = 0;
+
+
+///////////////////////////////////////////////////////////
+//
+// Global variables
+//
+
+// Static variables
+static HMODULE g_hModule = NULL;  // DLL module handle
+								  // Friendly name of component
+
+const char g_szFriendlyName[] = "Lab1 FN_01 Component";
+// Version-independent ProgID
+const char g_szVerIndProgID[] = "FN_01.Component";
+// ProgID
+const char g_szProgID[] = "FN_01.Component.1";
+
+
+
+
 
 STDAPI DllGetClassObject(REFCLSID rclsid, REFIID riid, void** ppv)
 {
@@ -45,3 +66,24 @@ STDAPI DllCanUnloadNow(void)
 	else
 		return(S_OK);
 }
+
+//
+// Server registration
+//
+STDAPI DllRegisterServer()
+{
+	return RegisterServer(g_hModule,
+		CLSID_CFN_01,
+		g_szFriendlyName,
+		g_szVerIndProgID,
+		g_szProgID);
+}
+
+
+STDAPI DllUnregisterServer()
+{
+	return UnregisterServer(CLSID_CFN_01,
+		g_szVerIndProgID,
+		g_szProgID);
+}
+
